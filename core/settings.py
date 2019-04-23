411fe91ce65351e0e3dd187b4a52b8682ea4ccc0
Your Name <you@example.com>
2019-04-23 21:10:33 +0000

    fix settings

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
